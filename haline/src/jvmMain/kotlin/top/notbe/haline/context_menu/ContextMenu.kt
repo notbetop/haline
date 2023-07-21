5a8e396e759db98f84fc6a6be897d72bf7f50b04
林发平 <wwalkingg@outlook.com>
2023-07-21 18:34:40 +0900

    aaa

--- haline/src/jvmMain/kotlin/top/notbe/haline/context_menu/ContextMenu.kt
+++ haline/src/jvmMain/kotlin/top/notbe/haline/context_menu/ContextMenu.kt
@@ -1,14 +1,23 @@
 package top.notbe.haline.context_menu
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.PointerMatcher
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.onClick
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerButton
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun ContextMenu(
+fun WithContextMenuScope.ContextMenu(
     modifier: Modifier = Modifier,
     content: @Composable ContextMenuScope.() -> Unit
 ) {
-
+    Box(modifier = Modifier.size(50.dp).background(Color.Blue))
 }
 
 @Composable
@@ -19,25 +28,25 @@ fun ContextMenuItem(
 
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun WithContextMenu(
     modifier: Modifier = Modifier,
-    content: @Composable ContextMenuScope.() -> Unit
+    contextMenu: @Composable WithContextMenuScope.() -> Unit,
+    content: @Composable () -> Unit
 ) {
-
+    var isContextMenuVisible by remember { mutableStateOf(false) }
+    Box(modifier = Modifier.onClick(
+        enabled = true,
+        matcher = PointerMatcher.mouse(PointerButton.Secondary), // add onClick for every required PointerButton
+        keyboardModifiers = { true }, // e.g { isCtrlPressed }; Remove it to ignore keyboardModifiers
+        onClick = {
+            isContextMenuVisible = true
+        }
+    )) {
+        content()
+    }
 }
 
-
+interface WithContextMenuScope
 interface ContextMenuScope
-
-//fun main() {
-//    ContextMenu {
-//        ContextMenuItem {
-//
-//        }
-//
-//        ContextMenuItem {
-//
-//        }
-//    }
-//}
---
+++ haline/src/jvmMain/kotlin/top/notbe/haline/context_menu/WithContextMenu.kt
@@ -0,0 +1,11 @@
+package top.notbe.haline.context_menu
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.LocalContextMenuRepresentation
+import androidx.compose.foundation.PointerMatcher
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.onClick
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerButton
+
